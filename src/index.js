513ad29c5d7243aca15a86d93ef01918e8a3d4ba
PatricioJulian <pj.patriciojulian@gmail.com>
2019-01-01 16:58:34 +0000

    code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
-const topsis = require('aopifyjs');
+const agent = require('aopifyjs');
+const linearAlgebra = require('linear-algebra')();
+const Matrix = linearAlgebra.Matrix; 
 const topsis = require('topsis');
+const ahp = require('ahp-lite');
 
+let m = new Matrix([
+                    [2, 5, 5], 
+                    [60, 26, 4], 
+                    [20, 20, 4],  
+                    [500, 2, 4], 
+                    [50, 23, 3], 
+                    [25, 10, 1]
+                    ]); 
+                    
+w = [];
+
+// w = ahp.getWeights(c).ev;
+
+// let a = topsis.getBest(m, w, ia);
+
+const ai = new Agent('ai');
+const human = new Agent('human');
+
+ai.start();
+human.start();
+
+
+ai.listen({name: 'request'}, human, () => { 
+  
+  ai.tell({name: 'response', msg: ''}, human);
+
+});
