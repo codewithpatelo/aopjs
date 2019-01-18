63e873cb796cb139de82e035fb73e08d858bb0d8
patelotech <pj.patriciojulian@gmail.com>
2019-01-18 18:43:27 +0100

    fix async function order

--- src/index.js
+++ src/index.js
@@ -23,58 +23,66 @@ const m = new Matrix([
 let w = [];
 
 
-
 // Criteria matrix array
 let cma = [];
 
 // Extracting data from csv
-
 const csvFilePath = './src/cm.csv';
 
 csv()
   .fromFile(csvFilePath)
   .then((jsonObj) => {
+  // Convert it into a usable matrix array.
+    cma = convert.toMatrixArray(jsonObj, 'linear-algebra');
+    return cma;
+  })
+  .then((cma) => {
       
-  //Convert it into a usable matrix array. 
-     cma = convert.toMatrixArray(jsonObj, 'linear-algebra');
+    const results = [];
+    // We calculate the eigenvector for each matrix
+    for (let i = 0; i < cma.length; i += 1) {
+      results.push(ahp.getWeights(cma[i]));
+    }
 
+    return results;
+  })
+  .then((results) => {
+    const wa = [];
+
+    // We check which assessment was a consistent judgement.
+    for (i = 0; i < cma.length; i += 1) {
+      if (results[i].cr > 0.20) {
+        wa.push(results[i].w);
+      }
+    }
+
+    return wa;
+  })
+  .then((wa) => {
+    // We sintetize judgement using geometric mean.
+    for (i = 0; i < wa.length; i += 1) {
+      w *= wa;
+    }
+
+
+    //w = Math.pow(w, 1/wa);
+    console.log(w);
+    return w;
   });
 
-let results = [];
-
-// We calculate the eigenvector for each matrix
-for (let i = 0; i < cma.length; i += 1) {
-    results.push(ahp.getWeights(cma[i])); 
-}
-
-let wa = [];
-
-//We check which assessment was consistent judgement.
-for (i = 0; i < cma.length; i += 1) {
-    if ( 0.20 < results[i].cr  ) {
-        wa.push(results[i].w);
-    }
-}
-
-// We sintetize judgement using geometric mean.
-for (i = 0; i < wa.length; i += 1) {
-    w = w * wa;
-}
 
 
-//w = Math.pow(w, 1/wa);
 
-let ia = ['max', 'min', 'min'];
-let arguments = {'m': m, 'w': w, 'ia' : ia};
 
-console.log(w);
-console.log(topsis.getBest(m, w, ia));
+const ia = ['max', 'min', 'min'];
+// let arguments = {'m': m, 'w': w, 'ia' : ia};
+
 
 
 const ai = new Agent('ai');
 const human = new Agent('human');
 
-//console.log(ai);
+// console.log(ai);
 
 ai.start();
 human.start();
@@ -82,25 +90,23 @@ human.start();
 /*
 
 ai.listen({ name: 'request' }, human, () => {
-    
-  topsis.getBest(m, w, ia);
-  
-  
-  
-  //ai.decide(topsis, arguments);
-    
-  ai.tell({ name: 'response', msg: 'The best fare for you is this one. The rating is '+msg[2]+' stars. You will reach location in around '+msg[1]+' minutes and the cost is '+msg[0]+' birrs.' }, human);
-  
-  });
 
+  topsis.getBest(m, w, ia);
+
+
+  //ai.decide(topsis, arguments);
+
+  ai.tell({ name: 'response', msg: 'The best fare for you is this one. The rating is '+msg[2]+' stars. You will reach location in around '+msg[1]+' minutes and the cost is '+msg[0]+' birrs.' }, human);
+
+  });
 
 
 human.tell({ name: 'request'}, ai);}
 
 ai.listen({ name: 'request' }, human, () => {
-    
-    console.log('Response succesful. The agent has helped the human!'); 
-    
+
+    console.log('Response succesful. The agent has helped the human!');
+
     });
 
 
